76c41380dd336d5c8f2d75f006234ad13d7b0668
phongsathorn2540 <37927965+phongsathorn2540@users.noreply.github.com>
2020-03-27 14:41:48 +0700

    add page register

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+const { check, validationResult } = require('express-validator');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/register' , function(req, res, next){
+  res.render('register' , {title: 'test insert'});
+});
+
+
+router.post('/register' ,
+  [ 
+    check("name" , "เห้ยใส่ข้อมูลก่อนกด input ดิวะ").not().isEmpty() 
+  ] , function(req, res, next){
+    const result = validationResult(req);
+    var errors = result.errors;
+    if(!result.isEmpty()) {
+      res.render('register' , {errors:errors} );
+    }else{
+      console.log(req.body.test);
+    }
 });
 
 module.exports = router;
